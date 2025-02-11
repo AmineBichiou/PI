d6f7c55b2aa2ef2beab1abf1d62277c2482d8977
nesrinelamine2021@gmail.com <nesrinelamine2021@gmail.com>
2025-02-11 09:15:11 -1100

    Votre message de commit ici

---
+++ migrations/Version20250207224447.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250207224447 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE evenement ADD type VARCHAR(20) NOT NULL, DROP datedebut, DROP datefin, DROP lieu');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE evenement ADD datedebut DATE NOT NULL, ADD datefin DATE NOT NULL, ADD lieu VARCHAR(255) NOT NULL, DROP type');
+    }
+}
