130736dfdffa11ebbd0a186eaf81934095924c62
nesrinelamine2021@gmail.com <nesrinelamine2021@gmail.com>
2025-02-16 04:29:58 -1100

    detail evenement front

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -12,69 +12,67 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use App\Repository\EvenementRepository; 
+use App\Repository\EvenementRepository;
 
 final class EvenementController extends AbstractController
-{     
+{
     // Afficher la liste des événements
     #[Route('/evenement', name: 'app_evenement')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        
         $evenements = $entityManager->getRepository(Evenement::class)->findAll();
 
-        
         return $this->render('evenement/index.html.twig', [
             'evenements' => $evenements,  
         ]);
-    }    
-
-    // Voir un événement
-#[Route('/evenement/{id}', name: 'app_evenement_voir')]
-public function voir(Evenement $evenement): Response
-{
-    return $this->render('evenement/voir.html.twig', [
-        'evenement' => $evenement,
-    ]);
-}
-
-// Ajouter un événement avec stockage de la photo dans le répertoire
-    #[Route('/evenement/ajout', name: 'app_evenement_ajout')]
-    public function ajout(Request $request, EntityManagerInterface $entityManager, ParameterBagInterface $params): Response
-{
-    $evenement = new Evenement();
-    $form = $this->createForm(EvenementType::class, $evenement);
-    $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-        // Gestion de la photo
-        $photoFile = $form->get('photoFile')->getData();
-        if ($photoFile) {
-            $photoFilename = uniqid() . '.' . $photoFile->guessExtension();
-            $photoFile->move(
-                $params->get('photos_directory'),
-                $photoFilename
-            );
-            $evenement->setPhoto($photoFilename);
-        }
-
-        // Gestion des régions
-        $regions = $form->get('regions')->getData();
-        foreach ($regions as $region) {
-            $evenement->addRegion($region); // Utilise la méthode addRegion de l'entité Evenement
-        }
-
-        $entityManager->persist($evenement);
-        $entityManager->flush();
-
-        $this->addFlash('success', 'Événement ajouté avec succès !');
-        return $this->redirectToRoute('app_evenement');
     }
 
-    return $this->render('evenement/ajout.html.twig', [
-        'form' => $form->createView(),
-    ]);
-}
+    // Voir un événement
+    #[Route('/evenement/{id}', name: 'app_evenement_voir')]
+    public function voir(Evenement $evenement): Response
+    {
+        return $this->render('evenement/voir.html.twig', [
+            'evenement' => $evenement,
+        ]);
+    }
+
+    // Ajouter un événement avec stockage de la photo dans le répertoire
+    #[Route('/evenement/ajout', name: 'app_evenement_ajout')]
+    public function ajout(Request $request, EntityManagerInterface $entityManager, ParameterBagInterface $params): Response
+    {
+        $evenement = new Evenement();
+        $form = $this->createForm(EvenementType::class, $evenement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Gestion de la photo
+            $photoFile = $form->get('photoFile')->getData();
+            if ($photoFile) {
+                $photoFilename = uniqid() . '.' . $photoFile->guessExtension();
+                $photoFile->move(
+                    $params->get('photos_directory'),
+                    $photoFilename
+                );
+                $evenement->setPhoto($photoFilename);
+            }
+
+            // Gestion des régions
+            $regions = $form->get('regions')->getData();
+            foreach ($regions as $region) {
+                $evenement->addRegion($region); // Utilise la méthode addRegion de l'entité Evenement
+            }
+
+            $entityManager->persist($evenement);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Événement ajouté avec succès !');
+            return $this->redirectToRoute('app_evenement');
+        }
+
+        return $this->render('evenement/ajout.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 
     // Modifier un événement avec gestion de la photo
     #[Route('/evenement/modifier/{id}', name: 'app_evenement_modifier')]
@@ -125,14 +123,22 @@ public function voir(Evenement $evenement): Response
         return $this->redirectToRoute('app_evenement');
     }
 
+    // Liste des événements (front)
+    #[Route('/evenements/liste', name: 'app_evenements_liste')]
+    public function listEvenements(EvenementRepository $evenementRepository): Response
+    {
+        $evenements = $evenementRepository->findAll();
+        return $this->render('evenement/liste.html.twig', [
+            'evenements' => $evenements,
+        ]);
+    }
 
-
-// front
-#[Route('/evenements/liste', name: 'app_evenements_liste')]
-public function listEvenements(EvenementRepository $evenementRepository): Response
-{
-    $evenements = $evenementRepository->findAll();
-    return $this->render('evenement/liste.html.twig', [
-        'evenements' => $evenements,
-    ]);
-}}
+    // Détail d'un événement
+    #[Route('/evenements/{id}', name: 'app_evenement_detail')]
+    public function detail(Evenement $evenement): Response
+    {
+        return $this->render('evenement/detail.html.twig', [
+            'evenement' => $evenement,
+        ]);
+    }
+}
